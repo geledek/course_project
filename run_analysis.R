25ed301411a334936bffe6b3fb7a94c1b4d028c8
han <geledek@gmail.com>
2015-03-23 00:05:54 +0800

    output added

--- run_analysis.R
+++ run_analysis.R
@@ -5,60 +5,44 @@
 ## 4. Appropriately labels the data set with descriptive activity names.
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-library("data.table")
-library("reshape2")
+require("plyr")
+require("data.table")
+require("reshape2")
 
 # Load: activity labels and column names
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+activity_labels <- read.table("../UCI HAR Dataset/activity_labels.txt")
 
-# Extract only the measurements on the mean and standard deviation for each measurement.
-extract_features <- grepl("mean|std", features)
+features <- read.table("../UCI HAR Dataset/features.txt", colClasses = c("character"))
 
-# Load and process X_test & y_test data.
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(read.table("../UCI HAR Dataset/test/subject_test.txt"),
+              read.table("../UCI HAR Dataset/test/y_test.txt"),
+              read.table("../UCI HAR Dataset/test/x_test.txt"))
 
-names(X_test) <- features
+train <- cbind(read.table("../UCI HAR Dataset/train/subject_train.txt"),
+              read.table("../UCI HAR Dataset/train/y_train.txt"),
+              read.table("../UCI HAR Dataset/train/x_train.txt"))
 
-# Extract only the measurements on mean and std
-X_test <- X_test[,extract_features]
+merged_data <- rbind(test, train)
 
-# Load lables and binding
-y_test[,2] <- activity_labels[y_test[,1]]
-names(y_test) <- c("Activity_ID", "Activity_Label")
-names(subject_test) <- "subject"
-test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+col_names <- c(c("subject", "activity.id"),features$V2)
 
-# Load training data sets
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+names(merged_data) <- col_names
 
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+merged_data_extract <- merged_data[, grepl("subject|activity|mean|std", names(merged_data))]
 
-names(X_train) = features
+merged_data_extract$activity.id <- activity_labels$V2[merged_data_extract$activity.id]
 
-# Extract only the measurements on the mean and standard deviation for each measurement.
-X_train = X_train[,extract_features]
+names(merged_data_extract) <- gsub('activity.id',"activity",names(merged_data_extract))
+names(merged_data_extract) <- gsub('Acc',"Acceleration",names(merged_data_extract))
+names(merged_data_extract) <- gsub('GyroJerk',"AngularAcceleration",names(merged_data_extract))
+names(merged_data_extract) <- gsub('Gyro',"AngularSpeed",names(merged_data_extract))
+names(merged_data_extract) <- gsub('Mag',"Magnitude",names(merged_data_extract))
+names(merged_data_extract) <- gsub('^t',"TimeDomain.",names(merged_data_extract))
+names(merged_data_extract) <- gsub('^f',"FrequencyDomain.",names(merged_data_extract))
+names(merged_data_extract) <- gsub('\\.mean',".Mean",names(merged_data_extract))
+names(merged_data_extract) <- gsub('\\.std',".StandardDeviation",names(merged_data_extract))
+names(merged_data_extract) <- gsub('Freq\\.',"Frequency.",names(merged_data_extract))
+names(merged_data_extract) <- gsub('Freq$',"Frequency",names(merged_data_extract))
 
-# Load activity data
-y_train[,2] = activity_labels[y_train[,1]]
-names(y_train) = c("Activity_ID", "Activity_Label")
-names(subject_train) = "subject"
-
-# Bind data
-train_data <- cbind(as.data.table(subject_train), y_train, X_train)
-
-# Merge test and train data
-data = rbind(test_data, train_data)
-
-id_labels   <- c("subject", "Activity_ID", "Activity_Label")
-data_labels <- setdiff(colnames(data), id_labels)
-melt_data   <- melt(data, id = id_labels, measure.vars = data_labels)
-
-# Apply mean function to dataset using dcast function
-tidy_data   <- dcast(melt_data, subject + Activity_Label ~ variable, mean)
-
-write.table(tidy_data, file = "./tidy_data.txt")
-
+merged_data_extract_avg = ddply(merged_data_extract, c("subject","activity"), numcolwise(mean))
+write.table(merged_data_extract_avg, file = "tidy_data.txt")
